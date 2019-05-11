c1b15257e3bbd7aa7bcafd0108369a3164172327
Remco Bravenboer <r.bravenboer1996@gmail.com>
2019-05-11 15:12:41 +0200

    commit

--- src/ParallelSelectionSort.java
+++ src/ParallelSelectionSort.java
@@ -1,26 +1,48 @@
 import java.util.Arrays;
-import java.util.Random;
 
 public class ParallelSelectionSort {
     private final static int CORES = 2;
-    private final static int NUMBERS = 3;
-    private static int x = 0;
+    private final static int NUMBERS = 200;
+    private static int arrayToUse[];
+    private static int splitArray[][];
 
     public static void main(String[] args) {
         int[] numbers = Numbers.generateNumber(NUMBERS);
         System.out.println("Starting numbers: " + Arrays.toString(numbers));
 
         for (int i = 0; i < NUMBERS; i++) {
-            System.out.print("\nNumbers (iteration " + i +"): ");
-            for (int j = i; j < NUMBERS; j++) {
-                //Use j for numbers index
-                System.out.print(numbers[j] + " ");
+            arrayToUse = Arrays.copyOfRange(numbers, i, numbers.length);
+            splitArray = fillSplitArray(CORES, arrayToUse);
+            System.out.println(Arrays.deepToString(splitArray));
+
+            for (int j = 0; j < CORES; j++) {
+                Thread t = new Thread(new ParallelSelectionSortRunnable(splitArray[j]));
+                t.start();
             }
         }
+    }
 
-        for (int i = 0; i < CORES; i++) {
-            Thread t = new Thread(new ParallelSelectionSortRunnable(numbers));
-            t.start();
+    private static int[][] fillSplitArray(int arrayAmount, int[] arrayToUse) {
+        if (arrayToUse.length == 0) {
+            return new int[0][0];
         }
+
+        int splitLength = (int) Math.ceil((double) arrayToUse.length / (double) arrayAmount);
+        int[][] splits = new int[arrayAmount][];
+
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i < arrayToUse.length; i++) {
+            if (k == splitLength) {
+                k = 0;
+                j++;
+            }
+            if (splits[j] == null) {
+                int remainingNumbers = arrayToUse.length - i;
+                splits[j] = new int[Math.min(remainingNumbers, splitLength)];
+            }
+            splits[j][k++] = arrayToUse[i];
+        }
+        return splits;
     }
 }
--- src/ParallelSelectionSortRunnable.java
+++ src/ParallelSelectionSortRunnable.java
@@ -1,8 +1,11 @@
 import java.util.Arrays;
 
+import static java.lang.Thread.sleep;
+
 public class ParallelSelectionSortRunnable implements Runnable {
 
     private int[] numbers;
+    private int lowestNumberInArray;
 
     public ParallelSelectionSortRunnable(int[] numbers) {
         this.numbers = numbers;
@@ -10,10 +13,27 @@ public class ParallelSelectionSortRunnable implements Runnable {
 
     public void run() {
         try {
+            int lowest = getLowestValue(this.numbers);
+            System.out.println("The lowest number in this array is " + lowest);
+
             System.out.print("\nThread " + Thread.currentThread().getId()
                     + " is running with data: " + Arrays.toString(this.numbers));
+
+
         } catch (Exception e) {
             System.out.println("The following went wrong: " + e);
         }
     }
+
+    private int getLowestValue(int[] numbers) {
+        lowestNumberInArray = Integer.MAX_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < lowestNumberInArray) {
+                lowestNumberInArray = numbers[i];
+            }
+        }
+        return lowestNumberInArray;
+    }
+
 }
